ddcf4a6b0f15695d829e056dde5388eafd3edeaa
Станислав Горожанкин <sgorozhankin@gmail.com>
2018-11-28 14:32:00 +0200

    add class student

---
+++ FacultyComputerScience/FacultyComputerScience/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacultyComputerScience
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+
+        public Student(string firstName, string secondName, int age)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            Age = age;
+        }
+    }
+}
