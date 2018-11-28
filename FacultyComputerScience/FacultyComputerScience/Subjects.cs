67e5c043a6daa82152d3773d6e6b63368997566b
sabnack <sgorozhankin@gmail.com>
2018-11-28 18:37:13 +0200

    add class subjects

---
+++ FacultyComputerScience/FacultyComputerScience/Subjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacultyComputerScience
+{
+    class Subjects
+    {
+        public string SubjectName;
+        public int HoursAmount;
+
+        public Subjects(string subjectName, int hoursAmount)
+        {
+            SubjectName = subjectName;
+            HoursAmount = hoursAmount;
+        }
+    }
+}
