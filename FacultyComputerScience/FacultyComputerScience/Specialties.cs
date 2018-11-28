e239f5298026792d392227b96b7ae60b6d5bc359
sabnack <sgorozhankin@gmail.com>
2018-11-28 19:11:43 +0200

    add methods add subjects and groups

--- FacultyComputerScience/FacultyComputerScience/Specialties.cs
+++ FacultyComputerScience/FacultyComputerScience/Specialties.cs
@@ -8,16 +8,25 @@ namespace FacultyComputerScience
 {
     class Specialties
     {
-        public List<Groups> Specialty = new List<Groups>();
+        public List<Groups> Groups = new List<Groups>();
         public List<Subjects> Subject = new List<Subjects>();
         public readonly string SpecialtyName;
 
         public Specialties(Groups group, Subjects subject, string specialtyName)
         {
-            Specialty.Add(group);
+            Groups.Add(group);
+            Subject.Add(subject);
             SpecialtyName = specialtyName;
         }
 
-        
+        public void AddGroup (Groups group)
+        {
+            Groups.Add(group);
+        }
+
+        public void AddSubject(Subjects subject)
+        {
+            Subject.Add(subject);
+        }
     }
 }
